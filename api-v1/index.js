8d708a2411d9638db63942ccefb2b9aa016437c2
Travis Fischer <fisch0920@gmail.com>
2018-04-17 21:15:35 -0400

    .

---
+++ api-v1/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Router = require('koa-router')
+const compose = require('koa-compose')
+const middleware = require('../lib/middleware')
+
+const {
+  auth,
+  me
+} = middleware
+
+const pub = new Router()
+const pri = new Router()
+
+pub.pst = pub.post
+pri.pst = pri.post
+
+pub.use(middleware.handleMongodbErrors)
+
+module.exports = () => compose([
+  pub.middleware(),
+  auth(), me(),
+  pri.middleware()
+])
